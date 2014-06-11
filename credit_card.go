af82a74e3e5b322cb0005ccf789f12311c2d69ff
Jonathan Azoff <jon@azoffdesign.com>
2014-06-10 19:02:49 -0700

    json and schema support for credit cards

--- credit_card.go
+++ credit_card.go
@@ -1,50 +1,50 @@
 package braintree
 
 type CreditCard struct {
-	CustomerId                string             `xml:"customer-id,omitempty"`
-	Token                     string             `xml:"token,omitempty"`
-	Number                    string             `xml:"number,omitempty"`
-	ExpirationDate            string             `xml:"expiration-date,omitempty"`
-	ExpirationMonth           string             `xml:"expiration-month,omitempty"`
-	ExpirationYear            string             `xml:"expiration-year,omitempty"`
-	CVV                       string             `xml:"cvv,omitempty"`
-	VenmoSDKPaymentMethodCode string             `xml:"venmo-sdk-payment-method-code,omitempty"`
-	VenmoSDK                  bool               `xml:"venmo-sdk,omitempty"`
-	Options                   *CreditCardOptions `xml:"options,omitempty"`
-	CreatedAt                 string             `xml:"created-at,omitempty"`
-	UpdatedAt                 string             `xml:"updated-at,omitempty"`
-	Bin                       string             `xml:"bin,omitempty"`
-	CardType                  string             `xml:"card-type,omitempty"`
-	CardholderName            string             `xml:"cardholder-name,omitempty"`
-	CustomerLocation          string             `xml:"customer-location,omitempty"`
-	ImageURL                  string             `xml:"image-url,omitempty"`
-	Default                   bool               `xml:"default,omitempty"`
-	Expired                   bool               `xml:"expired,omitempty"`
-	Last4                     string             `xml:"last-4,omitempty"`
-	Commercial                string             `xml:"commercial,omitempty"`
-	Debit                     string             `xml:"debit,omitempty"`
-	DurbinRegulated           string             `xml:"durbin-regulated,omitempty"`
-	Healthcare                string             `xml:"healthcare,omitempty"`
-	Payroll                   string             `xml:"payroll,omitempty"`
-	Prepaid                   string             `xml:"prepaid,omitempty"`
-	CountryOfIssuance         string             `xml:"country-of-issuance,omitempty"`
-	IssuingBank               string             `xml:"issuing-bank,omitempty"`
-	UniqueNumberIdentifier    string             `xml:"unique-number-identifier,omitempty"`
-	BillingAddress            *Address           `xml:"billing-address,omitempty"`
-	Subscriptions             *Subscriptions     `xml:"subscriptions,omitempty"`
+	CustomerId                string             `schema:"-" json:"-" xml:"customer-id,omitempty"`
+	Token                     string             `schema:"-" json:"-" xml:"token,omitempty"`
+	Number                    string             `schema:"number,omitempty" json:"number,omitempty" xml:"number,omitempty"`
+	ExpirationDate            string             `schema:"expiration_date" json:"expiration_date" xml:"expiration-date,omitempty"`
+	ExpirationMonth           string             `schema:"-" json:"-" xml:"expiration-month,omitempty"`
+	ExpirationYear            string             `schema:"-" json:"-" xml:"expiration-year,omitempty"`
+	CVV                       string             `schema:"cvv" json:"cvv" xml:"cvv,omitempty"`
+	VenmoSDKPaymentMethodCode string             `schema:"-" json:"-" xml:"venmo-sdk-payment-method-code,omitempty"`
+	VenmoSDK                  bool               `schema:"-" json:"-" xml:"venmo-sdk,omitempty"`
+	Options                   *CreditCardOptions `schema:"-" json:"-" xml:"options,omitempty"`
+	CreatedAt                 string             `schema:"-" json:"-" xml:"created-at,omitempty"`
+	UpdatedAt                 string             `schema:"-" json:"-" xml:"updated-at,omitempty"`
+	Bin                       string             `schema:"-" json:"-" xml:"bin,omitempty"`
+	CardType                  string             `schema:"-" json:"-" xml:"card-type,omitempty"`
+	CardholderName            string             `schema:"-" json:"-" xml:"cardholder-name,omitempty"`
+	CustomerLocation          string             `schema:"-" json:"-" xml:"customer-location,omitempty"`
+	ImageURL                  string             `schema:"-" json:"-" xml:"image-url,omitempty"`
+	Default                   bool               `schema:"-" json:"-" xml:"default,omitempty"`
+	Expired                   bool               `schema:"-" json:"-" xml:"expired,omitempty"`
+	Last4                     string             `schema:"last_4,omitempty" json:"last_4,omitempty" xml:"last-4,omitempty"`
+	Commercial                string             `schema:"-" json:"-" xml:"commercial,omitempty"`
+	Debit                     string             `schema:"-" json:"-" xml:"debit,omitempty"`
+	DurbinRegulated           string             `schema:"-" json:"-" xml:"durbin-regulated,omitempty"`
+	Healthcare                string             `schema:"-" json:"-" xml:"healthcare,omitempty"`
+	Payroll                   string             `schema:"-" json:"-" xml:"payroll,omitempty"`
+	Prepaid                   string             `schema:"-" json:"-" xml:"prepaid,omitempty"`
+	CountryOfIssuance         string             `schema:"-" json:"-" xml:"country-of-issuance,omitempty"`
+	IssuingBank               string             `schema:"-" json:"-" xml:"issuing-bank,omitempty"`
+	UniqueNumberIdentifier    string             `schema:"-" json:"-" xml:"unique-number-identifier,omitempty"`
+	BillingAddress            *Address           `schema:"-" json:"-" xml:"billing-address,omitempty"`
+	Subscriptions             *Subscriptions     `schema:"-" json:"-" xml:"subscriptions,omitempty"`
 }
 
 type CreditCards struct {
-	CreditCard []*CreditCard `xml:"credit-card"`
+	CreditCard []*CreditCard `schema:"-" json:"-" xml:"credit-card"`
 }
 
 type CreditCardOptions struct {
-	VerifyCard                    bool   `xml:"verify-card,omitempty"`
-	VenmoSDKSession               string `xml:"venmo-sdk-session,omitempty"`
-	MakeDefault                   bool   `xml:"make-default,omitempty"`
-	FailOnDuplicatePaymentMethod  bool   `xml:"fail-on-duplicate-payment-method,omitempty"`
-	VerificationMerchantAccountId string `xml:"verification-merchant-account-id,omitempty"`
-	UpdateExistingToken           string `xml:"update-existing-token,omitempty"`
+	VerifyCard                    bool   `schema:"-" json:"-" xml:"verify-card,omitempty"`
+	VenmoSDKSession               string `schema:"-" json:"-" xml:"venmo-sdk-session,omitempty"`
+	MakeDefault                   bool   `schema:"-" json:"-" xml:"make-default,omitempty"`
+	FailOnDuplicatePaymentMethod  bool   `schema:"-" json:"-" xml:"fail-on-duplicate-payment-method,omitempty"`
+	VerificationMerchantAccountId string `schema:"-" json:"-" xml:"verification-merchant-account-id,omitempty"`
+	UpdateExistingToken           string `schema:"-" json:"-" xml:"update-existing-token,omitempty"`
 }
 
 // AllSubscriptions returns all subscriptions for this card, or nil if none present.
